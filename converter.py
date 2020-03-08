b0092a0ab2cd586b6a0d518e090fc665119b8fac
karim007 <sellami.karim@hotmail.be>
2020-03-08 23:13:22 +0100

    update splitting process

--- converter.py
+++ converter.py
@@ -12,9 +12,11 @@ def hot_encoding(labels, digits):
 
 def split_and_shuffle_training_test( data, label_new):
     # m = data.shape[0] //2
-    m = data.shape[0] -1000
+    m = data.shape[0] -2000
+    
    
     data_train, data_test = data[:m].T, data[m:].T
+
     labels_train, labels_test = label_new[:,:m], label_new[:,m:]
 
     shuffle_index = np.random.permutation(m)
@@ -26,27 +28,37 @@ def split_and_shuffle_training_test( data, label_new):
 def reshapping_dimension(data):
     return data.reshape((len(data),np.prod(data.shape[1:])))
 
-data_folder="data/persian_letters"
+data_folder="data/my_mnist"
 
 data=[]
 labels=[]
+data_test=[]
+labels_test=[]
 
 classes=len(os.listdir(data_folder))
 label_code = open("label_code.txt", "w+")
 
 print("Start transformation")
-
+i=0
 for idx, d in enumerate(os.listdir(data_folder)):
-    
-    label_code.write("{} : {} ".format(d, idx))
-    label_code.write("\n")
-    print("Processsing {}".format(d))
-    for f in os.listdir("{}/{}".format(data_folder,d)):
-        
-        img = Image.open("{}/{}/{}".format(data_folder,d,f))
-        arr = np.array(img)
-        data.append(arr)
-        labels.append(idx)
+
+    if(os.path.isdir("{}/{}".format(data_folder,d))):
+        print("Processsing {}".format(d))
+        for f in os.listdir("{}/{}".format(data_folder,d)):
+            
+            if i % 10 != 0:
+                img = Image.open("{}/{}/{}".format(data_folder,d,f))
+                arr = np.array(img)
+                data.append(arr)
+                labels.append(d)
+            else:
+                img = Image.open("{}/{}/{}".format(data_folder,d,f))
+                arr = np.array(img)
+                data_test.append(arr)
+                labels_test.append(d)
+            i=i+1
+
+
 
 label_code.close()
 
@@ -57,12 +69,17 @@ print("Convert data to numpy arrays")
 data=np.array(data)
 labels=np.array(labels)
 
+data_test=np.array(data_test)
+labels_test=np.array(labels_test)
+
 print("Reshaping data")
-data=reshapping_dimension(data)
+data=reshapping_dimension(data).T
+data_test=reshapping_dimension(data_test).T
 labels_hot_encoded= hot_encoding(labels, classes)
+labels_test_hot_encoded= hot_encoding(labels_test, classes)
 
 print("Suffle and split data")
-data, labels, data_test, labels_test = split_and_shuffle_training_test(data,labels_hot_encoded)
+# data, labels, data_test, labels_test = split_and_shuffle_training_test(data,labels_hot_encoded)
 
 
 output_folder="output"
@@ -77,9 +94,9 @@ if not os.path.exists("{}/{}".format(output_folder, test_folder)):
     os.makedirs("{}/{}".format(output_folder, test_folder))
 
 print("Save numpy arrays")
-np.save('output/training/label.npy', labels)
+np.save('output/training/label.npy', labels_hot_encoded)
 np.save('output/training/data.npy', data)
-np.save('output/test/label.npy', labels_test)
+np.save('output/test/label.npy', labels_test_hot_encoded)
 np.save('output/test/data.npy', data_test)
 
 print("End script")
