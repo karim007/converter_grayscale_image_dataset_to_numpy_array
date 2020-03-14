8d652d6f8481064560de41b6329f565542199808
karim007 <sellami.karim@hotmail.be>
2020-03-14 10:29:44 +0100

    issue shuffle feature

--- converter.py
+++ converter.py
@@ -10,20 +10,29 @@ def hot_encoding(labels, digits):
     label_new = label_new.T.reshape(digits, examples)  
     return label_new
 
-def split_and_shuffle_training_test( data, label_new):
-    # m = data.shape[0] //2
-    m = data.shape[0] -2000
-    
-   
-    data_train, data_test = data[:m].T, data[m:].T
 
-    labels_train, labels_test = label_new[:,:m], label_new[:,m:]
+
+def shufflle( array,m):
 
     shuffle_index = np.random.permutation(m)
+    array = array[:, shuffle_index]
 
-    data_train, labels_train = data_train[:, shuffle_index], labels_train[:, shuffle_index]
+    return array
 
-    return data_train, labels_train, data_test, labels_test
+# def split_and_shuffle_training_test( data, label_new):
+#     # m = data.shape[0] //2
+#     m = data.shape[0] -2000
+    
+   
+#     data_train, data_test = data[:m].T, data[m:].T
+
+#     labels_train, labels_test = label_new[:,:m], label_new[:,m:]
+
+#     shuffle_index = np.random.permutation(m)
+
+#     data_train, labels_train = data_train[:, shuffle_index], labels_train[:, shuffle_index]
+
+#     return data_train, labels_train, data_test, labels_test
 
 def reshapping_dimension(data):
     return data.reshape((len(data),np.prod(data.shape[1:])))
@@ -40,17 +49,16 @@ label_code = open("label_code.txt", "w+")
 
 print("Start transformation")
 i=0
-for idx, d in enumerate(os.listdir(data_folder)):
+for idx, d  in enumerate(os.listdir(data_folder)):
 
     if(os.path.isdir("{}/{}".format(data_folder,d))):
         print("Processsing {}".format(d))
         for f in os.listdir("{}/{}".format(data_folder,d)):
-            
-            if i % 10 != 0:
+            if i % 100 != 0:
                 img = Image.open("{}/{}/{}".format(data_folder,d,f))
                 arr = np.array(img)
                 data.append(arr)
-                labels.append(d)
+                labels.append(d)            
             else:
                 img = Image.open("{}/{}/{}".format(data_folder,d,f))
                 arr = np.array(img)
@@ -78,6 +86,11 @@ data_test=reshapping_dimension(data_test).T
 labels_hot_encoded= hot_encoding(labels, classes)
 labels_test_hot_encoded= hot_encoding(labels_test, classes)
 
+# print("Shuffle data")
+# data= shufflle(data.T,data.shape[0])
+# data_test= shufflle(data_test.T,data_test.shape[0])
+# labels_hot_encoded=shufflle(labels_hot_encoded, labels_hot_encoded.shape[1])
+# labels_test_hot_encoded=shufflle(labels_test_hot_encoded,labels_test_hot_encoded.shape[1])
 
 
 output_folder="output"
